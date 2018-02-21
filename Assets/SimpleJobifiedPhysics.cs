652eec09b2711ce7b7bf7cc1e26bf729f3fc69de
Lotte <lotte@genericevil.com>
2018-02-21 12:02:09 +0000

    initial commit of demo code

---
+++ Assets/SimpleJobifiedPhysics.cs
@@ -0,0 +1,303 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class SimpleJobifiedPhysics : MonoBehaviour
+{
+    // When passing a data collection into a Unity Job it has to be one of the new Unity Native types.
+    // .net genrics (like List<T>) and plain arrays (like T[]) wil not work in jobs! 
+    private NativeArray<Vector3> velocities;
+    private NativeArray<Vector3> positions;
+    private NativeArray<Matrix4x4> renderMatices;
+    private NativeArray<int> sleeping;
+    private Vector3 gravity;
+    private int objectCount = 1023; // the most we can fit into a single call to Graphics.DrawInstance
+
+    public Mesh mesh;
+    public Material material;
+
+    public Transform spawnDirection;
+
+    // This job applies gravity to each objects vecocity in the simulation
+    struct GravityJob : IJobParallelFor
+    {
+        public float DeltaTime;
+
+        public Vector3 Gravity;
+        public NativeArray<Vector3> Velocities;
+        public NativeArray<int> Sleeping;
+
+        public void Execute(int i)
+        {
+            // has the object been marked as sleeping?
+            // sleeping objects are object that have settled, so they dont
+            // move at all in this case we skip adding gravity to make them
+            // appear more stable
+            if (Sleeping[i] > 0)
+            {
+                Velocities[i] = new Vector3(0, 0, 0);
+            }
+
+            // simply intergrate gravity based on time since last step.
+            Velocities[i] += Gravity * DeltaTime;
+        }
+    }
+
+    // this job creates a batch of RaycastCommands we are going to use for
+    // collision detection against the world. these can be sent to PhysX
+    // as a batch that will be executed in a job, rather than us having to
+    // call Physics.Raycast in a loop just on the main thread!
+    struct PrepairRaycastCommands : IJobParallelFor
+    {
+        public float DeltaTime;
+
+        public NativeArray<RaycastCommand> Raycasts;
+        [ReadOnly]
+        public NativeArray<Vector3> Velocities;
+        [ReadOnly]
+        public NativeArray<Vector3> Positions;
+
+
+        public void Execute(int i)
+        {
+            // figure out how far the object we are testing collision for
+            // wants to move in total. Our collision raycast only needs to be
+            // that far.
+            float distance = (Velocities[i] * DeltaTime).magnitude;
+            Raycasts[i] = new RaycastCommand(Positions[i], Velocities[i], distance);
+        }
+    }
+
+    // Intergrate the velocity into all the objects positions. We use the
+    // Raycast hit data to decide how much of the velocity to intergrate -
+    // we dont want to tunnel though the colliders in the scene!
+    struct IntergratePhysics : IJobParallelFor
+    {
+        public float DeltaTime;
+        [ReadOnly]
+        public NativeArray<Vector3> Velocities;
+        [ReadOnly]
+        public NativeArray<RaycastCommand> Raycasts;
+        [ReadOnly]
+        public NativeArray<RaycastHit> Hits;
+        public NativeArray<int> Sleeping;
+        public NativeArray<Vector3> Positions;
+
+        public void Execute(int i)
+        {
+            if (Sleeping[i] > 0) // if the object is sleeping, we dont have to intergrate anything
+            {
+                Sleeping[i]++;
+                return;
+            }
+
+            if (Hits[i].normal == Vector3.zero)
+            {
+                Positions[i] += Velocities[i] * (Velocities[i] * DeltaTime).magnitude;
+            }
+            else
+            {
+                Positions[i] = Hits[i].point + new Vector3(0, 0.1f, 0);
+            }
+        }
+    }
+
+    // Respond to collisions
+    struct CalculateCollisionResponse : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<RaycastHit> Hits;
+
+        public NativeArray<Vector3> Velocities;
+        public NativeArray<int> Sleeping;
+
+        public void Execute(int i)
+        {
+            // If there has been a collision, the RaycastHit normal will be
+            // non zero. When we know there has been a collisoin we can
+            // respond to it.
+            if (Hits[i].normal != Vector3.zero)
+            {
+                // first, lets check if the velocity has got very low before this collision. if so
+                // we are going to put the object to sleep. this will stop it from
+                // reciveing gravity and will make the simulation appear more stable
+                if (Velocities[i].magnitude <= 2f)
+                {
+                    Velocities[i] = Vector3.zero;
+                    Sleeping[i]++; // increment the sleeping counter. any value over 1 is sleeping. We can use this to respawn objects after they have been static for while
+                }
+                // lets implement a very simple bounce effect - just be reflecting the velocity by the
+                // collison normal (the normal is a vector pointing perpendicular away from the surface
+                // that we have hit).
+                // were also going to apply a damping effect to the velocity by removing force from the resulting
+                // reflected velocity. You can think of this as a really simple simulation of force lost
+                // due to friction against the surface we have collided with.
+                else
+                {
+                    Velocities[i] = Vector3.Reflect(Velocities[i], Hits[i].normal);
+
+                    var angleBetweenNormalAndUp = Vector3.Angle(Hits[i].normal, Vector3.up); // returns a value between 0-180
+                    var lerp = angleBetweenNormalAndUp / 180f;
+                    Velocities[i] = Velocities[i] * Mathf.Lerp(0.5f, 1f, lerp);
+                }
+            }
+        }
+    }
+
+    // Calculate and store a matrix we can use to draw each object. As we dont support rotation in this demo, we pretty
+    // much only supply the position. A fixed scale is also applied to make sure the meshes we draw arnt giant.
+    struct CalculateDrawMatricies : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<Vector3> positions;
+        public NativeArray<Matrix4x4> renderMatices;
+
+        public void Execute(int i)
+        {
+            renderMatices[i] = Matrix4x4.TRS(positions[i], Quaternion.identity, new Vector3(0.5f, 0.5f, 0.5f));
+        }
+    }
+
+    IEnumerator Start()
+    {
+        // lets wait for half a second before we set off the system -- just to avoid any stutters
+        yield return new WaitForSeconds(0.5f);
+
+        // lets define an rough approximation for gravity
+        gravity = new Vector3(0, -9f, 0);
+
+        // and set up all our NativeArrays with initial values
+        velocities = new NativeArray<Vector3>(objectCount, Allocator.Persistent);
+        positions = new NativeArray<Vector3>(objectCount, Allocator.Persistent);
+        sleeping = new NativeArray<int>(objectCount, Allocator.Persistent);
+        renderMatices = new NativeArray<Matrix4x4>(objectCount, Allocator.Persistent);
+
+        for (int i = 0; i < objectCount; i++)
+        {
+            Respawn(i);
+        }
+    }
+
+    private void Respawn(int i)
+    {
+    //velocities[i] = Random.onUnitSphere * Random.Range(2, 10); // spawn objects with velocities spreading them out in a sphere
+        velocities[i] = (Random.onUnitSphere + (((spawnDirection.position - transform.position).normalized).normalized)*1.3f).normalized * Random.Range(2f, 10f); // spawn with velocities arching towards a target object
+        positions[i] = transform.position;
+        sleeping[i] = 0;
+        renderMatices[i] = Matrix4x4.identity;
+    }
+
+    void Update()
+    {
+        // only do an update if the data has been initialized
+        if (!positions.IsCreated)
+            return;
+
+        var deltaTime = Time.deltaTime;
+
+        // FORCE ACCUMILATION
+
+        // First off lets apply gravity to all the object in our scene that arnt currently asleep
+        var gravityJob = new GravityJob()
+        {
+            DeltaTime = deltaTime,
+            Gravity = gravity,
+            Velocities = velocities,
+            Sleeping = sleeping
+        };
+        var gravityDependency = gravityJob.Schedule(objectCount, 32);
+
+        // TODO accumilate other forces here! gravity is just the simplest force to apply to our objects,
+        // but theres no limit on the kind of forces we can simulate. We could add friction, air resistnace,
+        // constant acceleration, joints and constrainsts, boyancy.. anything we can think of that can effect
+        // the velocity of an object can have its own job scheduled here.
+
+        // INTERGRATION AND COLLISION
+
+        // Create temporary arrays for the raycast info. Lets use the TempJob allocator,
+        // this means we have to dispose them when the job has finished - its short lived data
+        var raycastCommands = new NativeArray<RaycastCommand>(objectCount, Allocator.TempJob);
+        var raycastHits = new NativeArray<RaycastHit>(objectCount, Allocator.TempJob);
+
+        // Lets schedule jobs to do a collision raycast for each object. One job prepairs all the raycast commands,
+        // the second actually does the raycasts.
+        var setupRaycastsJob = new PrepairRaycastCommands()
+        {
+            DeltaTime = deltaTime,
+            Positions = positions,
+            Raycasts =  raycastCommands,
+            Velocities = velocities
+        };
+
+        var setupDependancy = setupRaycastsJob.Schedule(objectCount, 32, gravityDependency);
+        var raycastDependancy = RaycastCommand.ScheduleBatch(raycastCommands, raycastHits, 32, setupDependancy);
+
+        // Now we know if there is a collision along our velocity vector, its time to intergrate the velocity into
+        // our objects for the current timeset.
+        var intergrateJob = new IntergratePhysics()
+        {
+            DeltaTime = deltaTime,
+            Positions = positions,
+            Velocities = velocities,
+            Raycasts = raycastCommands,
+            Sleeping = sleeping,
+            Hits = raycastHits
+        };
+        var intergrateDependancy = intergrateJob.Schedule(objectCount, 32, raycastDependancy);
+
+        // finally, respond to any collisions that happened in the lsat update step.
+        var collisionResponeJob = new CalculateCollisionResponse()
+        {
+            Hits = raycastHits,
+            Velocities = velocities,
+            Sleeping = sleeping
+        };
+        var collisionDependacy = collisionResponeJob.Schedule(objectCount, 32, intergrateDependancy);
+
+        // Now the physics is done, we need to create a drawing matrix for every object. This simple demo dosnt
+        // implment roation, so only the translation values in the matrix reallllly matter.
+        var renderMatrixJob = new CalculateDrawMatricies()
+        {
+            positions = positions,
+            renderMatices = renderMatices
+        };
+        var matrixJob = renderMatrixJob.Schedule(objectCount, 32, collisionDependacy);
+
+        matrixJob.Complete();
+
+        // make sure we dispose of the temporary NativeArrays we used for raycasting
+        raycastCommands.Dispose();
+        raycastHits.Dispose();
+
+        // DEBUG - draw red lines showing object velocity
+        //for (int i = 0; i < objectCount; i++)
+        //{
+        ///   Debug.DrawLine(positions[i], positions[i] + velocities[i], Color.red, 0.016f, true);
+        //}
+
+        // Well, all the updating is done! lets actually issue a draw!
+        Graphics.DrawMeshInstanced(mesh, 0, material, renderMatices.ToArray());
+    
+        // finally lets respawn any object that has been asleep and stable for a while
+        // TODO as of Unity 2018.1b6, NativeArray is the only native collection type. When
+        // NativeQueue is included in the beta this last task can be jobified!
+
+        for (int i = 0; i < objectCount; i++)
+        {
+            if (sleeping[i] > 15)
+                Respawn(i);
+        }
+    }
+
+    private void OnDisable()
+    {
+        velocities.Dispose();
+        positions.Dispose();
+        renderMatices.Dispose();
+        sleeping.Dispose();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# upm-package-template-3DLegacy
+Project Template for 3D scenes not using a SRP 
+
+This template uses Unity’s built in rendering pipeline. This template does not contain any example content.
+
+
+Package Name: com.unity.template.3dempty
+Package Version: 0.0.1
